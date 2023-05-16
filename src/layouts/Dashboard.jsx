be8fe392e5fe18d2d1fbc82df5bcfe556a5ec6fb
kivancaksoy <kvnc96@hotmail.com>
2023-05-16 23:43:42 +0300

    Add JobSeekerList and jobSeekerService

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import Navi from './Navi'
+import JobSeekerList from '../pages/JobSeekerList'
+import { Grid } from "semantic-ui-react";
 
 export default function Dashboard() {
   return (
     <div>
-        
+        <Grid>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            Deneme
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <JobSeekerList/>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   )
 }
